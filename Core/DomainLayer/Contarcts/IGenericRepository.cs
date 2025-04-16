3179cb6cffab1855b3dcbc79a3a1077145d4e973
nada <nadamohamed242145@gmail.com>
2025-04-17 01:46:55 +0200

    3-Generic Repository

---
+++ Core/DomainLayer/Contarcts/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Contarcts
+{
+    public interface IGenericRepository<TEntity, Tkey> where TEntity : BaseEntity<Tkey>
+    {
+     Task<IEnumerable<TEntity>> GetAllAsync();  
+        Task<TEntity?> GetByIdAsync(Tkey id);
+        Task AddAsync(TEntity entity);  
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+
+    }
+}
