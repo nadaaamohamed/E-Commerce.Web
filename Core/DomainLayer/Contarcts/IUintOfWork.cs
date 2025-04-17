1d8856cbf9b12ddd2737ba9ae61197d2be48df5d
nada <nadamohamed242145@gmail.com>
2025-04-17 02:09:52 +0200

    4- UnitOfWork

---
+++ Core/DomainLayer/Contarcts/IUintOfWork.cs
@@ -0,0 +1,15 @@
+﻿using DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Contarcts
+{
+    public interface IUintOfWork
+    {
+        Task <int> SaveChangesAsync();
+        IGenericRepository<TEntity, Tkey> GenericRepository<TEntity, Tkey>() where TEntity : BaseEntity<Tkey>;
+    }
+}
