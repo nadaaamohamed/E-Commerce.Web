7402aec4af6579d797c98066d564a03410fe2a2a
Nada Mohamed <nadamohamed242145@gmail.com>
2025-04-28 04:06:19 +0300

    Revert "Session04"

--- infrastructure/Presistence/GlobalUsing.cs
+++
@@ -1,5 +0,0 @@
-﻿global using DomainLayer.Contarcts;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.Extensions.DependencyInjection;
-global using Presistence.Data;
-global using Presistence.Repositories;
